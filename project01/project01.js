df9dfaaa8d9699e4066976f5f059629533572f39
Andy Niccolai <aniccola@mines.edu>
2011-01-17 23:55:46 -0700

    fixed broken javascript and removed unneeded function

--- project01/project01.js
+++ project01/project01.js
@@ -1,14 +1,6 @@
 function validateForm() {
-	var str = document.getElementById('feeltoday').value;
+	var str = document.getElementById('feeling').value;
 	if(!str)
 		alert("Surely you feel something!");	
-	return false;
-	// uncomment to allow script to return if the form has been filled out
-	//}
-	//return true;
-}
-
-function clickLink(link) {
-	// link.id can be used to determine which button sent the event
 	return false;
 }
