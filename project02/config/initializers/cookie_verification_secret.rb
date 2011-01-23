98d904f7c756e3b340484d4e497a5b20b1ba8ab9
aniccola <aniccola@lakeshore.local>
2011-01-22 21:17:43 -0700

    add the default rails files for project 2

---
+++ project02/config/initializers/cookie_verification_secret.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random, 
+# no regular words or you'll be exposed to dictionary attacks.
+ActionController::Base.cookie_verifier_secret = '687195972942649055a0d29b2ff9a0d95663e127bf9c0972112ca4d7b4b4e1d67612b06cf2e5919b5d48aa12b37832d683fccee48cfd342274eaa8cd3090cf56';
