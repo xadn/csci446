7ad72858f06dcf6f8d35092e654a31787f8954be
Andy Niccolai <aniccola@mines.edu>
2011-02-02 05:16:48 -0700

    added cart and template

--- project02/app/controllers/store_controller.rb
+++ project02/app/controllers/store_controller.rb
@@ -1,6 +1,13 @@
 class StoreController < ApplicationController
+  
   def index
     @products = Product.find_products_for_sale
+  end
+  
+  def add_to_cart
+    product = Product.find(params[:id]) 
+    @cart = find_cart                   
+    @cart.add_product(product)          
   end
   
   private
