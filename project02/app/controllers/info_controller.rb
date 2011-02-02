6b88c40e150a3a32cf2bbd830b82537e275fee6b
Andy Niccolai <aniccola@mines.edu>
2011-02-02 07:27:49 -0700

    added some new files

--- project02/app/controllers/info_controller.rb
+++ project02/app/controllers/info_controller.rb
@@ -1,5 +1,15 @@
 class InfoController < ApplicationController
   def who_bought
+    @product = Product.find(params[:id])
+    @orders  = @product.orders
+    respond_to do |format|
+      format.xml { render :layout => false }
+    end
+  end
+
+  protected
+
+  def authorize
   end
 
 end
