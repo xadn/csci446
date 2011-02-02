f010072121b07d57f2334796492d08488ac1af01
Andy Niccolai <aniccola@mines.edu>
2011-02-02 04:51:43 -0700

    modified config to store session info in the database

--- project02/app/controllers/store_controller.rb
+++ project02/app/controllers/store_controller.rb
@@ -2,5 +2,11 @@ class StoreController < ApplicationController
   def index
     @products = Product.find_products_for_sale
   end
+  
+  private
+  
+    def find_cart
+      session[:cart] ||= Cart.new
+    end
 
 end
