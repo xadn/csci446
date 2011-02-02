da27114b3a31a1a7ba0bb25a7fc72dc67d1bff6a
Andy Niccolai <aniccola@mines.edu>
2011-02-02 07:31:12 -0700

    completed ch 12

--- project02/app/controllers/info_controller.rb
+++ project02/app/controllers/info_controller.rb
@@ -3,7 +3,8 @@ class InfoController < ApplicationController
     @product = Product.find(params[:id])
     @orders  = @product.orders
     respond_to do |format|
-      format.xml { render :layout => false }
+      format.html
+      format.atom { render :layout => false }
     end
   end
 
