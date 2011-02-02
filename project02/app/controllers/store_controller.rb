12cad3140d57c6d6c492a3a6d0738cee72b40937
Andy Niccolai <aniccola@mines.edu>
2011-02-02 05:50:30 -0700

    working on ajax cart

--- project02/app/controllers/store_controller.rb
+++ project02/app/controllers/store_controller.rb
@@ -2,12 +2,14 @@ class StoreController < ApplicationController
   
   def index
     @products = Product.find_products_for_sale
+    @cart = find_cart
   end
   
   def add_to_cart
     product = Product.find(params[:id]) 
     @cart = find_cart                   
     @cart.add_product(product)
+    redirect_to_index
   rescue
     logger.error("Attempt to access invalid product #{params[:id]}")
     flash[:notice] = "Invalid product"
@@ -21,6 +23,11 @@ class StoreController < ApplicationController
   end
   
   private
+  
+    def redirect_to_index(msg = nil)
+      flash[:notice] = msg if msg
+      redirect_to :action => 'index'
+    end
   
     def find_cart
       session[:cart] ||= Cart.new
