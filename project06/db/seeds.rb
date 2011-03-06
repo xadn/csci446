20a9508fa0c11db103cddd91f81b8ab735e38aa8
Andy Niccolai <aniccola@mines.edu>
2011-03-05 18:18:09 -0700

    added more seed data featuring jstalin and the gaminator

--- project06/db/seeds.rb
+++ project06/db/seeds.rb
@@ -17,6 +17,13 @@ User.create(
 		:password_confirmation => "password"
 	)
 	
+gaminator = User.create(
+		:username => "The Gaiminator",
+		:email => "hgaimur@mines.edu",
+		:password => "password",
+		:password_confirmation => "password"
+	)
+		
 fuzzy_rhino = User.create(
 		:username => "FuzzyTailedRhino",
 		:email => "frhino@mines.edu",
@@ -24,6 +31,27 @@ fuzzy_rhino = User.create(
 		:password_confirmation => "password"
 	)
 	
+mbates = User.create(
+		:username => "master-debater",
+		:email => "mbates@mines.edu",
+		:password => "password",
+		:password_confirmation => "password"
+	)
+
+jstalin2 = User.create(
+		:username => "jstalin2",
+		:email => "jstalin@mines.edu",
+		:password => "password",
+		:password_confirmation => "password"
+	)
+	
+kji = User.create(
+		:username => "KimJonginator",
+		:email => "kil@mines.edu",
+		:password => "password",
+		:password_confirmation => "password"
+	)
+		
 Game.create(
 		:user => fuzzy_rhino,
 		:title => "Zompocalypse",
@@ -41,3 +69,48 @@ Game.create(
 		:title => "Sharks with Lasers vs Zombies",
 		:rating => 4
 	)
+	
+Game.create(
+		:user => gaminator,
+		:title => "Deathcon Match 4",
+		:rating => 1
+	)
+		
+Game.create(
+	:user => gaminator,
+	:title => "Cooking Mama: Polish Food",
+	:rating => 1
+)
+
+Game.create(
+		:user => gaminator,
+		:title => "Ben Goes to the Zoo",
+		:rating => 3
+	)
+	
+Game.create(
+		:user => gaminator,
+		:title => "Ring Toss: Ultimate",
+		:rating => 3
+	)
+	
+Game.create(
+		:user => jstalin2,
+		:title => "Learn to Draw: Naked People",
+		:rating => 4
+)
+
+Game.create(
+		:user => kji,
+		:title => "Civilization IV",
+		:rating => 0
+	)
+	
+Game.create(
+		:user => jstalin2,
+		:title => "Jelly Blocs",
+		:rating => 4
+	)
+	
+
+	
